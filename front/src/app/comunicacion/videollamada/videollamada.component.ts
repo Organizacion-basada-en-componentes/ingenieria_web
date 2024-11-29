7a42de42fdb59843ba59216247b31a07bc069ee5
JuanSoM <jismbs@gmail.com>
2024-11-29 14:08:10 +0100

    videollamada

--- front/src/app/comunicacion/videollamada/videollamada.component.ts
+++ front/src/app/comunicacion/videollamada/videollamada.component.ts
@@ -8,5 +8,21 @@ import { Component } from '@angular/core';
   styleUrl: './videollamada.component.css'
 })
 export class VideollamadaComponent {
+  cameraOn = true;
+  microphoneOn = true;
 
+  toggleCamera() {
+    this.cameraOn = !this.cameraOn;
+    // Lógica para manejar la cámara (ej. habilitar/deshabilitar video)
+  }
+
+  toggleMicrophone() {
+    this.microphoneOn = !this.microphoneOn;
+    // Lógica para manejar el micrófono (ej. activar/desactivar audio)
+  }
+
+  endCall() {
+    // Lógica para finalizar la llamada
+    alert('Llamada finalizada');
+  }
 }
