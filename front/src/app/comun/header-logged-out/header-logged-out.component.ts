e3b0279a5937dd461ccc0ee9b0f4fda0de24e1ce
Diegodepab <depablodiegouma@gmail.com>
2024-12-16 19:43:57 +0100

    Registro e inicio mejor

--- front/src/app/comun/header-logged-out/header-logged-out.component.ts
+++ front/src/app/comun/header-logged-out/header-logged-out.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-header-logged-out',
   standalone: false,
@@ -7,12 +8,22 @@ import { Router } from '@angular/router';
   styleUrls: ['./header-logged-out.component.css']
 })
 export class HeaderLoggedOutComponent {
+  isMenuOpen = false; // Estado para el menú desplegable
+
   constructor(private router: Router) {}
 
   goToLogin(): void {
     this.router.navigate(['/login']);
+    this.isMenuOpen = false; // Cerrar el menú después de navegar
   }
+  
   goToRegister(): void {
     this.router.navigate(['/register']);
+    this.isMenuOpen = false; // Cerrar el menú después de navegar
+  }
+
+  // Función para alternar el menú desplegable
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 }
