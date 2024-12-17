176b00617c62b91190f7b6f1d1e68b007a3a5719
Diegodepab <depablodiegouma@gmail.com>
2024-12-17 02:37:22 +0100

    pov Paciente telefono mejorado, además se hizo mejoras en el login como mostrar mensajes de credenciales invalidas

--- front/src/app/paciente/header-logged-in-paciente/header-logged-in-paciente.component.ts
+++ front/src/app/paciente/header-logged-in-paciente/header-logged-in-paciente.component.ts
@@ -5,23 +5,48 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header-logged-in-paciente',
   standalone: false,
-  
   templateUrl: './header-logged-in-paciente.component.html',
-  styleUrl: './header-logged-in-paciente.component.css'
+  styleUrls: ['./header-logged-in-paciente.component.css']
 })
 export class HeaderLoggedInPacienteComponent {
-  
-  
-  constructor(private router: Router,@Inject(AuthService) private authService: AuthService) {}
-  
+  // Propiedad para controlar la visibilidad del menú
+  menuVisible: boolean = false;
+
+  constructor(private router: Router, @Inject(AuthService) private authService: AuthService) {}
+
+  // Función para cerrar sesión
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/']);
   }
+
+  // Función para redirigir a la página de rehabilitación
   goToRehabilitacion(): void {
     this.router.navigate(['rehabilitacion-paciente']);
   }
+  goTocomunication(): void {
+    this.router.navigate(['comunicacion']);
+  }
+
+  // Función para redirigir a la página de inicio
   goToHome(): void {
     this.router.navigate(['/home-paciente']);
+  }
+
+
+  // Método que navega a la ruta "/comunicacion"
+  goToCommunication() {
+    this.router.navigate(['/comunicacion']); // Navega a la ruta deseada
+  }
+  
+  // Función para alternar el estado del menú
+  toggleMenu(): void {
+    this.menuVisible = !this.menuVisible;
+    const headerActions = document.querySelector('.header-actions') as HTMLElement;
+    if (this.menuVisible) {
+      headerActions.classList.add('active');
+    } else {
+      headerActions.classList.remove('active');
+    }
   }
 }
