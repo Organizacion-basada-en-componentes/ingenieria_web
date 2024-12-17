2c13414f5c58b4bb6c2b570fa31e69e5129fb34b
Diegodepab <depablodiegouma@gmail.com>
2024-12-17 01:59:41 +0100

    pov medico desde telefono mejorado

--- front/src/app/medico/header-logged-in-medico/header-logged-in-medico.component.ts
+++ front/src/app/medico/header-logged-in-medico/header-logged-in-medico.component.ts
@@ -5,30 +5,38 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-header-logged-in-medico',
   standalone: false,
-  
   templateUrl: './header-logged-in-medico.component.html',
   styleUrl: './header-logged-in-medico.component.css'
 })
 export class HeaderLoggedInMedicoComponent {
+  menuVisible: boolean = false;
 
+  constructor(private router: Router, @Inject(AuthService) private authService: AuthService) {}
 
-    
- 
-    constructor(private router: Router,@Inject(AuthService) private authService: AuthService) {}
-  
-    logout(): void {
-      this.authService.logout();
-      this.router.navigate(['/']);
+  toggleMenu(): void {
+    this.menuVisible = !this.menuVisible;
+    const headerActions = document.querySelector('.header-actions') as HTMLElement;
+    if (this.menuVisible) {
+      headerActions.classList.add('active');
+    } else {
+      headerActions.classList.remove('active');
+    }
+  }
 
-    }
-    goToHome() {
-      this.router.navigate(['/home-medico']);
-    }
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/']);
+  }
 
-    goToComunicacion() {
-      this.router.navigate(['/comunicacion']);
-    }
-    goToRehabilitacion() {
-      this.router.navigate(['/rehabilitacion-medico']);
-    }
+  goToHome() {
+    this.router.navigate(['/home-medico']);
+  }
+
+  goToComunicacion() {
+    this.router.navigate(['/comunicacion']);
+  }
+
+  goToRehabilitacion() {
+    this.router.navigate(['/rehabilitacion-medico']);
+  }
 }
